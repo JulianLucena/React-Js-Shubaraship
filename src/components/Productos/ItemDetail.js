9a27bb76c28b92e32ab6e191a6a768651fc8b9b0
JulianLucena <julianlucena5@gmail.com>
2021-07-24 22:47:30 -0300

    Desafío 9 V 2.0

--- src/components/Productos/ItemDetail.js
+++ src/components/Productos/ItemDetail.js
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 import ItemCount from "./ItemCount";
+import { Link } from "react-router-dom";
 
 const ItemDetail = ({category, id, img, price, stock, title}) => {
 
     const [count, setCount] = useState(1);
+
+    const [finished, setFinished] = useState(false);
+
+    const handleState = () => setFinished(!finished);
 
     return(
         <div className="detailbox">
@@ -12,7 +17,19 @@ const ItemDetail = ({category, id, img, price, stock, title}) => {
                 <h1>{title}</h1>
                 <h2>Precio: ${price}</h2> 
                 <p>Cantidad disponible: {stock}</p>
-                <ItemCount stock={stock} count={count} setCount={setCount} />
+                {!finished ? (     
+                    <>               
+                    <ItemCount stock={stock} count={count} setCount={setCount} />
+                    <button onClick={handleState}>Comprar</button> 
+                    </>                   
+                ) : (
+                    <>
+                    <Link to="/cart" onClick={handleState}>
+                        <button onClick={handleState}>Terminar mi compra</button>
+                    </Link>
+                    <button onClick={handleState}>Modificar</button>
+                    </>
+                )}                
             </div>
         </div>
     );
