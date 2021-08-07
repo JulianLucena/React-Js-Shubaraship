825b9ee9abc8dd26b3184efd9431de1d2e13ad70
JulianLucena <julianlucena5@gmail.com>
2021-08-07 00:59:57 -0300

    Desafío 12 parte 2

--- src/components/Productos/ItemList.js
+++ src/components/Productos/ItemList.js
@@ -2,14 +2,17 @@ import React, { useContext } from "react"
 import { CartContext } from "../CartContext/CartContext"
 import Item from "./Item"
 
-export const ItemList = ( {items} ) => {
+export const ItemList = ( {figura} ) => {
 
     const {carrito} = useContext(CartContext)
     console.log(carrito)
 
     return (
         <div>
-            {items.map( producto => <Item key={producto.id} item={producto}/>)}
+            <img src={figura.img} alt={figura.nombre} />
+            <h1>{figura.nombre} (ID: {figura.id}) </h1>            
+            <h2>Precio: ${figura.precio}</h2>
+            <h3>Cantidad: {figura.stock}</h3>
         </div>
     )
 }
--- src/components/Productos/ItemListContainer.js
+++ src/components/Productos/ItemListContainer.js
@@ -1,58 +1,35 @@
 import React, { useEffect, useState } from "react";
 import ItemList from "./ItemList";
 import { useParams } from "react-router";
+import { database } from "../../firebase/firebase";
 
 const ItemListContainer = () => {
 
-    const [items, setItems] = useState([]);
+    const [figurasAMostrar, setFigurasAMostrar] = useState([]);
 
-    const { id } = useParams();
+    const obtenerFiguras = () => {
 
-    const itemsArray = [
-        {
-            id: 1,
-            title: "Funko Pop Figure: All Might",
-            img: "./img/All_Might.jpg",
-            category: 1,
-            price: 2500,
-            stock: 10,
-        },
-        {
-            id: 2,
-            title: "Funko Pop Figure: Halloween Town Sora",
-            img: "./img/Halloween_Sora.png",
-            category: 1,
-            price: 2500,
-            stock: 20,
-        },
-        {
-            id: 3,
-            title: "Funko Pop Figure: Queen",
-            img: "./img/Queen.png",
-            category: 1,
-            price: 2500,
-            stock: 15,
-        }
-    ]
+        const figuras = database.collection("Figuras");
 
-    const pedirProductos = () => {
-        return new Promise( (resolve, reject) =>{
-            setTimeout(() => {
-                resolve(itemsArray)
-            }, 2000)
-        })
+        figuras
+        .get()
+        .then((query) => 
+            setFigurasAMostrar(query.docs.map(doc => {
+                return {...doc.data(), id: doc.id };
+            }))
+        );
     }
 
-    useEffect(() => {
-        pedirProductos()
-        .then( res => {
-            console.log(res)
-            setItems(res)
-        })
-    }, [])
-
     return <div>
-        <ItemList items={items}/>
+
+        <button onClick={obtenerFiguras}>Llamar Firebase</button>
+
+        {figurasAMostrar.length ? (
+            figurasAMostrar.map(figura => <ItemList figura ={figura} />)
+        ) : (
+            <h3>Cargando...</h3>
+        )}
+        
     </div>
 };
 
