b80a5c275cca4e5c4dd112abfb6dec129c0c8500
JulianLucena <julianlucena5@gmail.com>
2021-08-13 19:22:50 -0300

    Más productos

--- src/components/Productos/CartItem.js
+++ src/components/Productos/CartItem.js
@@ -5,8 +5,8 @@ const CartItem = ({ item }) => {
 
     return(
         <div className="ItemStyleContainer card myCard">
-            <img className="StyleImg" src={item.img} alt= {item.title} />
             <span className="CardTitle">{item.nombre}</span>
+            <img className="StyleImg" src={item.img} alt= {item.title}/>            
             <span className="CardPrice">Precio: {item.precio}</span>      
             <span className="CardAmount">Cantidad: {item.cantidad}</span>  
             <span className="CardTotal">Total: {item.precio * item.cantidad}</span>
--- src/components/Productos/ItemList.js
+++ src/components/Productos/ItemList.js
@@ -9,11 +9,11 @@ export const ItemList = ( {producto} ) => {
 
     return (       
         <div className="card myCard">
-            <img className="StyleImg" src={producto.img} alt={producto.nombre} />
-            <span className="CardTitle">{producto.nombre}</span>            
+            <span className="CardTitle">{producto.nombre}</span>     
+            <img className="StyleImg" src={producto.img} alt={producto.nombre}/>                   
             <span className="CardPrice">Precio: ${producto.precio}</span>
             <span className="CardAmount">Cantidad: {producto.stock}</span>
-            <Link to={`/categoria/${producto.categoria}/${producto.id}`}><span className="CardMore">Ver más...</span></Link>
+            <Link className="CardMore" to={`/categoria/${producto.categoria}/${producto.id}`}><span>Comprar</span></Link>
         </div>
     )
 }
